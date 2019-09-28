747fcafeacd5e9c9efc80b70d0a0a3d898dfcee8
PRINCE ALI <ali.rehman@technyxsystems.com>
2019-09-28 09:33:35 +0500

    21stSep2019

--- Module-2/21stSep2019.py
+++ Module-2/21stSep2019.py
@@ -1,9 +1,28 @@
 import numpy
 
-def abc():
+def listToNDArray():
     list = [1,2,3,4]
+    list2 = [1,2,3,4]
+    list2 = numpy.array(list2)
+
     list = numpy.array(list)
 
-    return list*2
+    return list*list2
 
-print(abc())
+def arrayOfZeros():
+     return numpy.zeros(1000)
+
+def arrayOfOnes():
+    return numpy.ones(3)
+
+def arrayOfEmpty():
+    return numpy.empty(10)
+
+def numPyArange():
+    return numpy.arange(10)
+
+print(listToNDArray())
+print(arrayOfZeros())
+print(arrayOfOnes().shape)
+print(arrayOfEmpty())
+print(numPyArange())
